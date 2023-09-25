37bedd73d33109aeb4451d21ce37b416c8e66e4f
KovaDav <kovacs.david990420@gmail.com>
2023-09-25 10:10:47 +0200

    Feature: Desired image size can be inputted

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ def main():
     path = input("Enter the path to the image : \n")
     try:
         image = PIL.Image.open(path)
+        resize(image, int(input("Enter desired image size:")))
     except:
         print(path, "Unable to find image ");
 
-    return path
+
+ASCII_CHARS = ["@", "#", "$", "%", "?", "*", "+", ";", ":", ",", "."]
+
+def resize(image, new_width):
+    width, height = image.size
+    new_height = new_width * height / width
+    return image.resize((new_width, new_height))
+
+
 
 main()
