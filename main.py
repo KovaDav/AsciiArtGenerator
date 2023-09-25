d1ebe0b914ca239ae769aec1bd27a5f30474a1d0
KovaDav <kovacs.david990420@gmail.com>
2023-09-25 11:58:05 +0200

    Feature: Added braille character array and started working on 2d array for braille art

--- main.py
+++ main.py
@@ -7,19 +7,60 @@ def main():
 
     except:
         print(path, "Unable to find image ");
+        return
 
-    image = resize(image)
-    image = to_greyscale(image)
-    ascii_string = pixel_to_ascii(image)
-    ascii_img = ""
+    if(input("Braille or Ascii?") == "a"):
+        image = resize(image)
+        image = to_greyscale(image)
+        ascii_string = pixel_to_ascii(image)
+        ascii_img = ""
 
-    for i in range(0, len(ascii_string), image.width*2):
-        ascii_img += ascii_string[i:i+image.width*2] + "\n"
-    print(ascii_img)
+        for i in range(0, len(ascii_string), image.width*2):
+            ascii_img += ascii_string[i:i+image.width*2] + "\n"
+        print(ascii_img)
+    else:
+        image = to_greyscale(image)
+        pixels = image.getdata()
+        array = []
+        row = 0;
+
+        for i in range(0, int(image.height)):
+            array += [""]
+
+        array[0][0] += "asd"
+        print(array)
+        return
+
+        for pixel in pixels:
+            for i in range(1, image.width*image.height):
+                if(i % image.width == 0):
+                    row += 1
+                array[row] += str(pixel)
+        print(array)
+
 
 ASCII_CHARS = ["@", "#", "%", "?", "*", "+", "!", ";", ":", ",", "`"]
 
-def resize(image, new_width = 75):
+BRAILLE_CHARS = [
+  '⠀', '⠁', '⠂', '⠃', '⠄', '⠅', '⠆', '⠇', '⠈', '⠉', '⠊', '⠋', '⠌', '⠍', '⠎', '⠏',
+  '⠐', '⠑', '⠒', '⠓', '⠔', '⠕', '⠖', '⠗', '⠘', '⠙', '⠚', '⠛', '⠜', '⠝', '⠞', '⠟',
+  '⠠', '⠡', '⠢', '⠣', '⠤', '⠥', '⠦', '⠧', '⠨', '⠩', '⠪', '⠫', '⠬', '⠭', '⠮', '⠯',
+  '⠰', '⠱', '⠲', '⠳', '⠴', '⠵', '⠶', '⠷', '⠸', '⠹', '⠺', '⠻', '⠼', '⠽', '⠾', '⠿',
+  '⡀', '⡁', '⡂', '⡃', '⡄', '⡅', '⡆', '⡇', '⡈', '⡉', '⡊', '⡋', '⡌', '⡍', '⡎', '⡏',
+  '⡐', '⡑', '⡒', '⡓', '⡔', '⡕', '⡖', '⡗', '⡘', '⡙', '⡚', '⡛', '⡜', '⡝', '⡞', '⡟',
+  '⡠', '⡡', '⡢', '⡣', '⡤', '⡥', '⡦', '⡧', '⡨', '⡩', '⡪', '⡫', '⡬', '⡭', '⡮', '⡯',
+  '⡰', '⡱', '⡲', '⡳', '⡴', '⡵', '⡶', '⡷', '⡸', '⡹', '⡺', '⡻', '⡼', '⡽', '⡾', '⡿',
+  '⢀', '⢁', '⢂', '⢃', '⢄', '⢅', '⢆', '⢇', '⢈', '⢉', '⢊', '⢋', '⢌', '⢍', '⢎', '⢏',
+  '⢐', '⢑', '⢒', '⢓', '⢔', '⢕', '⢖', '⢗', '⢘', '⢙', '⢚', '⢛', '⢜', '⢝', '⢞', '⢟',
+  '⢠', '⢡', '⢢', '⢣', '⢤', '⢥', '⢦', '⢧', '⢨', '⢩', '⢪', '⢫', '⢬', '⢭', '⢮', '⢯',
+  '⢰', '⢱', '⢲', '⢳', '⢴', '⢵', '⢶', '⢷', '⢸', '⢹', '⢺', '⢻', '⢼', '⢽', '⢾', '⢿',
+  '⣀', '⣁', '⣂', '⣃', '⣄', '⣅', '⣆', '⣇', '⣈', '⣉', '⣊', '⣋', '⣌', '⣍', '⣎', '⣏',
+  '⣐', '⣑', '⣒', '⣓', '⣔', '⣕', '⣖', '⣗', '⣘', '⣙', '⣚', '⣛', '⣜', '⣝', '⣞', '⣟',
+  '⣠', '⣡', '⣢', '⣣', '⣤', '⣥', '⣦', '⣧', '⣨', '⣩', '⣪', '⣫', '⣬', '⣭', '⣮', '⣯',
+  '⣰', '⣱', '⣲', '⣳', '⣴', '⣵', '⣶', '⣷', '⣸', '⣹', '⣺', '⣻', '⣼', '⣽', '⣾', '⣿'
+]
+
+def resize(image, new_width = 50):
     width, height = image.size
     new_height = new_width * height / width
     return image.resize((int(new_width), int(new_height)))
