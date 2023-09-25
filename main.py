579b5c4dbf0050a882fce944a21956fc49262449
KovaDav <kovacs.david990420@gmail.com>
2023-09-25 11:11:14 +0200

    Refactor: changed some ascci characters, and doubled the width to height ratio so it looks better

--- main.py
+++ main.py
@@ -13,15 +13,13 @@ def main():
     ascii_string = pixel_to_ascii(image)
     ascii_img = ""
 
-    for i in range(0, len(ascii_string), image.width):
-        ascii_img += ascii_string[i:i+image.width] + "\n"
-
+    for i in range(0, len(ascii_string), image.width*2):
+        ascii_img += ascii_string[i:i+image.width*2] + "\n"
     print(ascii_img)
 
+ASCII_CHARS = ["@", "#", "%", "?", "*", "+", "!", ";", ":", ",", "`"]
 
-ASCII_CHARS = ["@", "#", "$", "%", "?", "*", "+", ";", ":", ",", "."]
-
-def resize(image, new_width = 100):
+def resize(image, new_width = 75):
     width, height = image.size
     new_height = new_width * height / width
     return image.resize((int(new_width), int(new_height)))
@@ -33,6 +31,7 @@ def pixel_to_ascii(image):
     pixels = image.getdata()
     ascii_str = "";
     for pixel in pixels:
+        ascii_str += ASCII_CHARS[pixel//25];
         ascii_str += ASCII_CHARS[pixel//25];
     return ascii_str
 
