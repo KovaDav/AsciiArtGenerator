0de69341c25edcffa49b0ae13e9173974a4a9b39
KovaDav <kovacs.david990420@gmail.com>
2023-09-25 10:28:53 +0200

    Feature: Pixeldata is accessible

--- main.py
+++ main.py
@@ -4,18 +4,23 @@ def main():
     path = input("Enter the path to the image : \n")
     try:
         image = PIL.Image.open(path)
-        resize(image, int(input("Enter desired image size:")))
+        image = resize(image)
+        image = to_greyscale(image)
+        pixels = image.getdata()
+
     except:
         print(path, "Unable to find image ");
 
 
+
 ASCII_CHARS = ["@", "#", "$", "%", "?", "*", "+", ";", ":", ",", "."]
 
-def resize(image, new_width):
+def resize(image, new_width = 100):
     width, height = image.size
     new_height = new_width * height / width
-    return image.resize((new_width, new_height))
+    return image.resize((int(new_width), int(new_height)))
 
-
+def to_greyscale(image):
+    return image.convert("L")
 
 main()
