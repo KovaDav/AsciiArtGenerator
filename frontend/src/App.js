446d86038126934657a23a114039842da65bb4ad
KovaDav <kovacs.david990420@gmail.com>
2023-10-03 11:01:08 +0200

    Feature: Python backend connected to react frontend with flask

---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+// Importing modules
+import React, { useState, useEffect } from "react";
+import "./App.css";
+ 
+function App() {
+    // usestate for setting a javascript
+    // object for storing and using data
+    const [data, setdata] = useState({
+        message: ""
+    });
+ 
+    // Using useEffect for single rendering
+    useEffect(() => {
+        // Using fetch to fetch the api from
+        // flask server it will be redirected to proxy
+        fetch("/data").then((res) =>
+            res.json().then((data) => {
+                // Setting a data from api
+                setdata({
+                    message: data.message,
+                });
+            })
+        );
+    }, []);
+ 
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>React and flask</h1>
+                {/* Calling a data from setdata for showing */}
+                <p>{data.message}</p>
+
+            </header>
+        </div>
+    );
+}
+ 
+export default App;
