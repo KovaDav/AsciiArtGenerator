a5bfe063acd2d7cfed71e7c95a3a2bed6ade0996
KovaDav <kovacs.david990420@gmail.com>
2024-02-27 10:31:36 +0100

    Update: pdfForm component has child elements

--- frontend/src/components/pdfForm.js
+++ frontend/src/components/pdfForm.js
@@ -3,7 +3,15 @@ import React, { useState, useEffect } from 'react';
 function PdfForm() {
     return(
         <div className='pdfForm'>
-            <p>sziauram</p>
+            <h2>PDF downloader options</h2>
+            <label for = 'PdfPaperSize'>Size of paper</label>
+            <input id='PdfPaperSize' type='dropdown'></input>
+            <label for = 'PdfPaperOrientation'>Orientation of paper</label>
+            <input id='PdfPaperOrientation' type='dropdown'></input>
+            <label for = 'PdfHorizontalSlider'>X starting coordinate</label>
+            <input type={"range"} min={"1"} max={"50"} id={"PdfHorizontalSlider"} className={"PdfSlider"}></input>
+            <label for = 'PdfVerticalSlider' className='PdfVerticalSliderDesc'>Y starting coordinate</label>
+            <input type={"range"} min={"1"} max={"50"} id={"PdfVerticalSlider"} className={"PdfSlider"}></input>
         </div>
     )
 }
