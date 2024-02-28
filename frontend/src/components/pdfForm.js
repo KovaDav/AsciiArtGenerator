47b574710b4e3b06a647331f4d8b91a479f19f48
KovaDav <kovacs.david990420@gmail.com>
2024-02-28 10:34:15 +0100

    Refactor: Paper size changed to checkboxes

--- frontend/src/components/pdfForm.js
+++ frontend/src/components/pdfForm.js
@@ -1,11 +1,34 @@
 import React, { useState, useEffect } from 'react';
 
+
+
 function PdfForm() {
     return(
         <div className='pdfForm'>
             <h2>PDF downloader options</h2>
-            <label for = 'PdfPaperSize'>Size of paper</label>
-            <input id='PdfPaperSize' type='dropdown'></input>
+            <h4>Paper size</h4>
+            <div id='paperSizeContainer'>
+                <section className='PdfCheckboxLabelContainer'>
+                <label className='PdfCheckboxLabel' for = 'A2Checkbox'>A2</label>
+                <input className='PdfCheckbox' id='A2Checkbox' type='checkbox'></input>
+                </section>
+                <section className='PdfCheckboxLabelContainer'>
+                <label className='PdfCheckboxLabel' for = 'A3Checkbox'>A3</label>
+                <input className='PdfCheckbox' id='A3Checkbox' type='checkbox'></input>
+                </section >
+                <section className='PdfCheckboxLabelContainer'>
+                <label className='PdfCheckboxLabel' for = 'A4Checkbox'>A4</label>
+                <input className='PdfCheckbox' id='A4Checkbox' type='checkbox'></input>
+                </section>
+                <section className='PdfCheckboxLabelContainer'>
+                <label className='PdfCheckboxLabel' for = 'A4Checkbox'>A5</label>
+                <input className='PdfCheckbox' id='A5Checkbox' type='checkbox'></input>
+                </section>
+                <section className='PdfCheckboxLabelContainer'>
+                <label className='PdfCheckboxLabel' for = 'A6Checkbox'>A6</label>
+                <input className='PdfCheckbox' id='A6Checkbox' type='checkbox'></input>
+                </section>
+            </div>
             <label for = 'PdfPaperOrientation'>Orientation of paper</label>
             <input id='PdfPaperOrientation' type='dropdown'></input>
             <label for = 'PdfHorizontalSlider'>X starting coordinate</label>
