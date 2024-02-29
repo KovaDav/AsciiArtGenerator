4c5051e334ad0ae80e11011ab87bdc0b4f172150
KovaDav <kovacs.david990420@gmail.com>
2024-02-29 10:36:02 +0100

    Feature: papersize and orientation functionality works

--- frontend/src/components/pdfForm.js
+++ frontend/src/components/pdfForm.js
@@ -8,40 +8,49 @@ import { font2 } from '../MonospaceTypewriter-normal';
 
 function PdfForm({pdfString, setPdfString, pdfType, ascii, braille, atkinson}) {
 
+    const [paperSize, setPaperSize] = useState("a4")
+    const [paperOrientation, setPaperOrientation] = useState("p")
+    const [paperCoordinateX, setPaperCoordinateX] = useState(1)
+    const [paperCoordinateY, setPaperCoordinateY] = useState(1)
+
 useEffect(() => {
+    handlePdf()
+},[paperSize,paperOrientation,paperCoordinateX,paperCoordinateY])
+
+const handlePdf = () => {
     const doc = new jsPDF({
-        orientation: "l",
+        orientation: paperOrientation,
         unit: "mm",
-        format: "a4",
+        format: paperSize,
       });
-      console.log(pdfType)
+      console.log(paperSize)
       if(pdfType === 'braille'){
         doc.addFileToVFS("BlistaBraille.ttf", font)
         doc.addFont("BlistaBraille-normal.ttf", "BlistaBraille", "normal")
         doc.setFont('BlistaBraille')
         doc.setLineHeightFactor(1)
         doc.setFontSize(10)
-        doc.text(braille,1,1)
+        doc.text(braille,paperCoordinateX,paperCoordinateY)
       }else if(pdfType === "atkinson"){
         doc.addFileToVFS("BlistaBraille.ttf", font)
         doc.addFont("BlistaBraille-normal.ttf", "BlistaBraille", "normal")
         doc.setFont('BlistaBraille')
         doc.setLineHeightFactor(1)
         doc.setFontSize(10)
-        doc.text(atkinson,1,1)
+        doc.text(atkinson,paperCoordinateX,paperCoordinateY)
       }else{
         doc.addFileToVFS("MonospaceTypewriter.ttf", font2)
         doc.addFont("MonospaceTypewriter-normal.ttf", "MonospaceTypewriter", "normal")
         doc.setFont('MonospaceTypewriter')
         doc.setLineHeightFactor(1)
         doc.setFontSize(10)
-        doc.text(ascii,1,1)
+        doc.text(ascii,paperCoordinateX,paperCoordinateY)
       }
 
       
-      setPdfString(doc.output('datauri'))
-      //doc.save('ascii.pdf')
-},[])
+      setPdfString(doc.output('datauristring'))
+      //doc.save("ascii.pdf")
+}
 
 
 
@@ -52,34 +61,34 @@ useEffect(() => {
             <div id='paperSizeContainer'>
                 <section className='PdfCheckboxLabelContainer'>
                 <label className='PdfCheckboxLabel' for = 'A2Checkbox'>A2</label>
-                <input className='PdfCheckbox' id='A2Checkbox' type='checkbox'></input>
+                <input className='PdfCheckbox' id='A2Checkbox' type='checkbox' checked={paperSize === "a2" ? true : false} onChange={() => {setPaperSize("a2")}}></input>
                 </section>
                 <section className='PdfCheckboxLabelContainer'>
                 <label className='PdfCheckboxLabel' for = 'A3Checkbox'>A3</label>
-                <input className='PdfCheckbox' id='A3Checkbox' type='checkbox'></input>
+                <input className='PdfCheckbox' id='A3Checkbox' type='checkbox' checked={paperSize === "a3" ? true : false} onChange={() => {setPaperSize("a3")}}></input>
                 </section >
                 <section className='PdfCheckboxLabelContainer'>
                 <label className='PdfCheckboxLabel' for = 'A4Checkbox'>A4</label>
-                <input className='PdfCheckbox' id='A4Checkbox' type='checkbox'></input>
+                <input className='PdfCheckbox' id='A4Checkbox' type='checkbox'  checked={paperSize === "a4" ? true : false} onChange={() => {setPaperSize("a4")}}></input>
                 </section>
                 <section className='PdfCheckboxLabelContainer'>
                 <label className='PdfCheckboxLabel' for = 'A4Checkbox'>A5</label>
-                <input className='PdfCheckbox' id='A5Checkbox' type='checkbox'></input>
+                <input className='PdfCheckbox' id='A5Checkbox' type='checkbox' checked={paperSize === "a5" ? true : false} onChange={() => {setPaperSize("a5")}}></input>
                 </section>
                 <section className='PdfCheckboxLabelContainer'>
                 <label className='PdfCheckboxLabel' for = 'A6Checkbox'>A6</label>
-                <input className='PdfCheckbox' id='A6Checkbox' type='checkbox'></input>
+                <input className='PdfCheckbox' id='A6Checkbox' type='checkbox' checked={paperSize === "a6" ? true : false} onChange={() => {setPaperSize("a6")}}></input>
                 </section>
             </div>
             <h4>Paper orientation</h4>
             <div id='paperOrientationContainer'>
                 <section className='PdfCheckboxLabelContainer'>
                 <img src={Vertical} alt="Vertical" className='PdfOrientationIcon'></img>
-                <input className='PdfCheckbox' id='A3Checkbox' type='checkbox'></input>
+                <input className='PdfCheckbox' id='A3Checkbox' type='checkbox' checked={paperOrientation === "p" ? true : false} onChange={() => {setPaperOrientation("p")}}></input>
                 </section>
                 <section className='PdfCheckboxLabelContainer'>
                 <img src={Horizontal} alt="Horizontal" className='PdfOrientationIcon'></img>
-                <input className='PdfCheckbox' id='A2Checkbox' type='checkbox'></input>
+                <input className='PdfCheckbox' id='A2Checkbox' type='checkbox' checked={paperOrientation === "l" ? true : false} onChange={() => {setPaperOrientation("l")}}></input>
                 </section>
             </div>
             <h4>PDF preview</h4>
