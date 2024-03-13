66225a8dc76878ecb7634c4892a5cd049c44fa61
KovaDav <kovacs.david990420@gmail.com>
2024-03-13 10:14:31 +0100

    Refactor: landing page done looks-wise

--- frontend/src/Pages/Layout/layout.jsx
+++ frontend/src/Pages/Layout/layout.jsx
@@ -1,9 +1,22 @@
 import { Outlet, Link } from "react-router-dom";
 import "./layout.css"
+import Button from '@mui/material-next/Button';
 
 const Layout = () => (
-    <div className="Layout">
-     Layout
+    <div className="Background">
+        <h1 className={"Header"}>ASCII ART GENERATOR</h1>
+        <div id="LandingPageButtonContainer">
+            <Button
+            className="LandingPageButton"
+            size="large"
+            variant="outlined">Download Ascii Art as PDF</Button>
+
+            <Button
+            className="LandingPageButton"
+            size="large"
+            variant="outlined">Just play around with Ascii Art</Button>
+        </div>  
+
     </div>
   );
   
