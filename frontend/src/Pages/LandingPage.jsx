6ae5b06922c88e25c1de3a5288206d9cf7a6c5a9
KovaDav <kovacs.david990420@gmail.com>
2024-03-13 09:46:14 +0100

    Refactor: base structure for the frontend built

---
+++ frontend/src/Pages/LandingPage.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+const landingPage = () => {
+
+
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default landingPage;
---
+++ frontend/src/Pages/Layout/layout.jsx
@@ -0,0 +1,10 @@
+import { Outlet, Link } from "react-router-dom";
+import "./layout.css"
+
+const Layout = () => (
+    <div className="Layout">
+     Layout
+    </div>
+  );
+  
+  export default Layout;
