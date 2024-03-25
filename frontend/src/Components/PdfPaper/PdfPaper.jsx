bd3542c38492acb7f8e5c9bc7d1162a240c34dcd
KovaDav <kovacs.david990420@gmail.com>
2024-03-25 10:21:51 +0100

    Refactor: pdf download page functionalities are working

--- frontend/src/Components/PdfPaper/PdfPaper.jsx
+++ frontend/src/Components/PdfPaper/PdfPaper.jsx
@@ -10,18 +10,92 @@ import TextField from '@mui/material/TextField';
 import "./PdfPaper.css"
 import Horizontal from '../../icons/horizontal.png';
 import Vertical from '../../icons/vertical.png';
+import { jsPDF } from 'jspdf';
+import { font } from '../../BlistaBraille-normal';
+import { font2 } from '../../MonospaceTypewriter-normal';
 
-const PdfPaper = ({colorInverted, setColorInverted, string, setReplace, replace, type, setBrightness, brightness, setWidth, setSelectedFile }) => {
+
+const PdfPaper = ({brightness, setWidth, setSelectedFile, ascii, braille, atkinson, setBrailleBrightness, setAtkinsonBrightness, invertColorsPdfDownloader, setPdfType, pdfType }) => {
     const [dropDownValue, setDropDownValue] = useState("")
     const [paperOrientation, setPaperOrientation] = useState("p")
     const [paperSize, setPaperSize] = useState("a4")
     const [paperCoordinateX, setPaperCoordinateX] = useState(1)
     const [paperCoordinateY, setPaperCoordinateY] = useState(1)
     const [pdfString, setPdfString] = useState("")
+    const [colorInverted, setColorInverted] = useState(false)
+
 
     const changeHandler = (event) => {
         setSelectedFile(event.target.files[0]);
     };
+
+    const handlePdf = (save) => {
+        const doc = new jsPDF({
+            orientation: paperOrientation,
+            unit: "mm",
+            format: paperSize,
+          });
+          if(pdfType === 'braille'){
+            doc.addFileToVFS("BlistaBraille.ttf", font)
+            doc.addFont("BlistaBraille-normal.ttf", "BlistaBraille", "normal")
+            doc.setFont('BlistaBraille')
+            doc.setLineHeightFactor(1)
+            doc.setFontSize(10)
+            if(colorInverted){
+            
+            doc.rect(0, 0, 150000, 150000, "F");
+            doc.setTextColor(255,255,255)
+            doc.setFillColor(0,0,0)
+            }
+            doc.text(braille,paperCoordinateX,paperCoordinateY)
+          }else if(pdfType === 'atkinson'){
+            doc.addFileToVFS("BlistaBraille.ttf", font)
+            doc.addFont("BlistaBraille-normal.ttf", "BlistaBraille", "normal")
+            doc.setFont('BlistaBraille')
+            doc.setLineHeightFactor(1)
+            doc.setFontSize(10)
+            if(colorInverted){
+            
+            doc.rect(0, 0, 150000, 150000, "F");
+            doc.setTextColor(255,255,255)
+            doc.setFillColor(0,0,0)
+            }
+            doc.text(atkinson,paperCoordinateX,paperCoordinateY)
+          }else{
+            doc.addFileToVFS("MonospaceTypewriter.ttf", font2)
+            doc.addFont("MonospaceTypewriter-normal.ttf", "MonospaceTypewriter", "normal")
+            doc.setFont('MonospaceTypewriter')
+            doc.setLineHeightFactor(1)
+            doc.setFontSize(10)
+            if(colorInverted){
+            
+            doc.rect(0, 0, 150000, 150000, "F");
+            doc.setTextColor(255,255,255)
+            doc.setFillColor(0,0,0)
+            }
+            doc.text(ascii,paperCoordinateX,paperCoordinateY)
+          }
+          
+          
+          setPdfString(doc.output('datauristring'))
+          if(save){
+              doc.save("ascii.pdf")
+          }
+    }
+    
+    const handleBrightnessChange = (brightness) => {
+        if(pdfType === 'braille'){
+            setBrailleBrightness(brightness)
+        }else if(pdfType === 'atkinson'){
+            setAtkinsonBrightness(brightness)
+        }
+    }
+
+    
+    useEffect(() => {
+        handlePdf(false)
+    },[ascii,braille,atkinson,paperSize,paperOrientation,paperCoordinateX,paperCoordinateY])
+
     return(
         <Paper  id="pdfDownloaderPaper">
 
@@ -31,10 +105,10 @@ const PdfPaper = ({colorInverted, setColorInverted, string, setReplace, replace,
                 id="demo-simple-select"
                 value={dropDownValue}
                 label="Character Type"
-                onChange={(e) => {setDropDownValue(e.target.value)}}>
-                <MenuItem value={"Ascii"}>Ascii</MenuItem>
-                <MenuItem value={"Braille"}>Braille</MenuItem>
-                <MenuItem value={"Atkinson-Braille"}>Atkinson-Braille</MenuItem>
+                onChange={(e) => {setPdfType(e.target.value);setDropDownValue(e.target.value)}}>
+                <MenuItem value={"ascii"}>Ascii</MenuItem>
+                <MenuItem value={"braille"}>Braille</MenuItem>
+                <MenuItem value={"atkinson"}>Atkinson-Braille</MenuItem>
             </Select>
             </div>
 
@@ -46,9 +120,9 @@ const PdfPaper = ({colorInverted, setColorInverted, string, setReplace, replace,
 
             <div className='FlexColumnContainerCentered pdfDownloaderSettingsContainer'>
                 <p className="NoMargin BoldText BrightnessLabel">Brightness</p>
-                <input type={"range"} min={"1"} max={"254"} defaultValue={brightness} id={"Slider"} onChange={e => setBrightness(e.target.value)}></input>
+                <input type={"range"} min={"1"} max={"254"} defaultValue={brightness} id={"Slider"} onChange={e => setTimeout(()=>{handleBrightnessChange(e.target.value)},500)}></input>
                 <p className="NoMargin BoldText">Color inverter</p>
-                <Switch id='ColorInverter' onClick={e => setColorInverted(!colorInverted)}/>
+                <Switch id='ColorInverter' onClick={e => {setColorInverted(!colorInverted); invertColorsPdfDownloader(pdfType)}}/>
             </div>
 
             <div className='FlexColumnContainerCentered pdfDownloaderSettingsContainer'>
@@ -93,7 +167,7 @@ const PdfPaper = ({colorInverted, setColorInverted, string, setReplace, replace,
             </section>
             <section className='pdfDownloaderVerticalSliderContainer'>
             <p id="pdfDownloaderVerticalDescription" className="BoldText">Vertical(Y) position on paper</p>
-            <input id="pdfDownloaderVerticalSlider" type={"range"} min={"1"} max={"350"} className={"PdfSlider"} onChange={e => setPaperCoordinateY(e.target.value)}></input>
+            <input id="pdfDownloaderVerticalSlider" type={"range"} min={"1"} max={"350"} className={"PdfSlider"} defaultValue={"1"} onChange={e => setPaperCoordinateY(e.target.value)}></input>
             </section>
             <iframe id="pdfDownloaderPreview" background="black" title='pdf' src={pdfString} allow='fullscreen'/>
                 
--- frontend/src/Pages/PdfDownloaderPage.jsx
+++ frontend/src/Pages/PdfDownloaderPage.jsx
@@ -8,24 +8,20 @@ import StringPaper from "../Components/StringPaper/StringPaper";
 import PdfPaper from "../Components/PdfPaper/PdfPaper"
 
 const PdfDownloaderPage = () => {
-  const [selectedFile, setSelectedFile] = useState(false)
-  const [width , setWidth] = useState(50)
-  const [ascii , setAscii] = useState("")
+ 	const [selectedFile, setSelectedFile] = useState(false)
+	const [width , setWidth] = useState(50)
+	const [ascii , setAscii] = useState("")
 	const [braille , setBraille] = useState("")
 	const [atkinson , setAtkinson] = useState("")
-  const [isBrailleSelected , setIsBrailleSelected] = useState(false)
-	const [isAtkinsonSelected , setIsAtkinsonSelected] = useState(false)
-	const [isAsciiSelected, setIsAsciiSelected] = useState(false)
-  const [brailleBrightness, setBrailleBrightness] = useState(128)
+  	const [brailleBrightness, setBrailleBrightness] = useState(128)
 	const [atkinsonBrightness, setAtkinsonBrightness] = useState(128)
 	const [asciiInverted, setAsciiInverted] = useState(false)
 	const [brailleInverted, setBrailleInverted] = useState(false)
 	const [atkinsonInverted, setAtkinsonInverted] = useState(false)
-	const [brailleReplace, setBrailleReplace] = useState(false)
-	const [atkinsonReplace, setAtkinsonReplace] = useState(false)
+	const [pdfType, setPdfType] = useState("")
 
   const handleSubmissionAscii = () => {
-		if(isAsciiSelected){    
+		if(pdfType === 'ascii'){    
 
 		const formData = new FormData();
 		formData.append('File', selectedFile);
@@ -49,13 +45,13 @@ const PdfDownloaderPage = () => {
 	};
 
   const handleSubmissionBraille = () => {
-		if(isBrailleSelected === true){
+		if(pdfType === 'braille'){
 			
 		const formData = new FormData();
 		formData.append('File', selectedFile);
 		fetch(
 			//`http://localhost:5000/braille?width=${width}&brightness=${brailleBrightness}&inverted=${brailleInverted}&replace=${brailleReplace}`
-			`https://KovaDav.eu.pythonanywhere.com/braille?width=${width}&brightness=${brailleBrightness}&inverted=${brailleInverted}&replace=${brailleReplace}`
+			`https://KovaDav.eu.pythonanywhere.com/braille?width=${width}&brightness=${brailleBrightness}&inverted=${brailleInverted}&replace=false`
 			,
 			{
 				method: 'POST',
@@ -73,13 +69,13 @@ const PdfDownloaderPage = () => {
 	};
 
 	const handleSubmissionAtkinson = () => {
-		if(isAtkinsonSelected === true){
+		if(pdfType === 'atkinson'){
 			
 		const formData = new FormData();
 		formData.append('File', selectedFile);
 		fetch(
 			//`http://localhost:5000/atkinson?width=${width}&brightness=${atkinsonBrightness}&inverted=${atkinsonInverted}&replace=${atkinsonReplace}`
-			`https://KovaDav.eu.pythonanywhere.com/atkinson?width=${width}&brightness=${atkinsonBrightness}&inverted=${atkinsonInverted}&replace=${atkinsonReplace}`
+			`https://KovaDav.eu.pythonanywhere.com/atkinson?width=${width}&brightness=${atkinsonBrightness}&inverted=${atkinsonInverted}&replace=false`
 			,
 			{
 				method: 'POST',
@@ -96,19 +92,31 @@ const PdfDownloaderPage = () => {
 		}
 	};
 
+	const invertColorsPdfDownloader = (type) => {
+		if(type === "ascii"){
+			setAsciiInverted(!asciiInverted)
+		}else if(type === "braille"){
+			setBrailleInverted(!brailleInverted)
+		}else if(type === "atkinson"){
+			setAtkinsonInverted(!atkinsonInverted)
+		}
+	}	
+
   useEffect(() => {
     if(selectedFile === false){
 		return
 	}
     handleSubmissionAscii()
-	  handleSubmissionBraille()
-	  handleSubmissionAtkinson()
+	handleSubmissionBraille()
+	handleSubmissionAtkinson()
     
-  }, [selectedFile,brailleBrightness,atkinsonBrightness,width,brailleReplace, atkinsonReplace,asciiInverted, brailleInverted, atkinsonInverted,isAsciiSelected, isBrailleSelected, isAtkinsonSelected]);
+  }, [selectedFile,brailleBrightness,atkinsonBrightness,width,asciiInverted, brailleInverted, atkinsonInverted, pdfType]);
     return(
      
         <div id="pdfDownloaderPaperContainer" className="FlexColumnContainerCentered">
-		<PdfPaper></PdfPaper>
+		<PdfPaper setSelectedFile={setSelectedFile} ascii={ascii} braille={braille} atkinson={atkinson}
+		 setBrailleBrightness={setBrailleBrightness} setAtkinsonBrightness={setAtkinsonBrightness} invertColorsPdfDownloader={invertColorsPdfDownloader}
+		 setWidth={setWidth} pdfType={pdfType} setPdfType={setPdfType}></PdfPaper>
         </div>
     )
 }
