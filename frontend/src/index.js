7d087cf50464796fd366ee9b0b6576208da29bd2
KovaDav <kovacs.david990420@gmail.com>
2024-05-05 15:55:02 +0200

    Bugfix: updated kinde's redirect and logour uri

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -43,8 +43,8 @@ root.render(
     <KindeProvider
 		clientId="af96ab8101f64fff9bb6951112211899"
 		domain="https://ascii.kinde.com"
-		redirectUri="http://localhost:3000"
-		logoutUri="http://localhost:3000"
+		redirectUri="https://ascii-art-generator-eight.vercel.app/"
+		logoutUri="https://ascii-art-generator-eight.vercel.app/"
 	>
     <RouterProvider router={router} />
     </KindeProvider>
