bfbb1aa5aedac98158fa72a43787bd2db1f68b41
KovaDav <kovacs.david990420@gmail.com>
2024-05-07 14:49:54 +0200

    Feature: vercel analytics test

--- frontend/src/Pages/Layout/layout.jsx
+++ frontend/src/Pages/Layout/layout.jsx
@@ -2,10 +2,12 @@ import { Outlet, Link } from "react-router-dom";
 import "./layout.css"
 import React, {useEffect, useState, useRef} from 'react';
 import Button from '@mui/material-next/Button';
+import { Analytics } from '@vercel/analytics/react';
 
 const Layout = () => (
     <div className="Background">
        <div id="HeaderAndLayoutPageButtonContainer" className="headerDisappear">
+       <Analytics />
        <h1 className={"Header"}>ASCII ART GENERATOR</h1>
         <div id="LayoutPageButtonContainer">
             <Link to="playaround">
