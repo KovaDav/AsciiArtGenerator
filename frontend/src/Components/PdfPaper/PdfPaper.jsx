a402fa9039616c8194072fcf8fea7aba8975ec3d
KovaDav <115537183+KovaDav@users.noreply.github.com>
2024-07-23 10:35:00 +0200

    testing pdf

--- frontend/src/Components/PdfPaper/PdfPaper.jsx
+++ frontend/src/Components/PdfPaper/PdfPaper.jsx
@@ -171,12 +171,8 @@ const PdfPaper = ({brightness, setWidth, setSelectedFile, ascii, braille, atkins
             <p id="pdfDownloaderVerticalDescription" className="BoldText">Vertical(Y) position on paper</p>
             <input id="pdfDownloaderVerticalSlider" type={"range"} min={"1"} max={"350"} className={"PdfSlider"} defaultValue={"1"} onChange={e => setPaperCoordinateY(e.target.value)}></input>
             </section>
-            {if (typeof window.Android !== 'undefined' && window.Android !== null) {
-            {<iframe id="pdfDownloaderPreview" background="black" title='pdf' src={pdfString} allow='fullscreen'/>}
-
-            }
-            }
-                
+            <iframe id="pdfDownloaderPreview" background="black" title='pdf' src={pdfString} allow='fullscreen'/>
+  
             <button onClick={e => handlePdf(true)}>save</button>
         </Paper>
     )
